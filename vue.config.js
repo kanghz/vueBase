54fd4c712b041e27615f07d26ee686a33d5637a7
YCKJ1943 <kanghaizhen@cloudwalk.cn>
2019-07-01 17:44:40 +0800

    添加第三方依赖单独打包

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,27 @@ module.exports = {
   devServer: {
     port: 3000,
     proxy: {
-      "/service":{
+      "/service": {
         "target": 'http://localhost:8090'
       }
     }
   },
   css: {
     extract: true
+  },
+  configureWebpack: {
+    optimization: {
+      minimize: false,
+      splitChunks: {
+        chunks: 'initial',
+        cacheGroups: {
+            commons: {
+                test: /[\\/]node_modules[\\/]/,
+                name: "common",
+                chunks: "all"
+            }
+        }
+      }
+    },
   }
 }
