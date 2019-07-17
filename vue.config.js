47e087903d6a58dd163ef3c475a3038775acef6f
YCKJ1943 <kanghaizhen@cloudwalk.cn>
2019-07-17 17:15:36 +0800

    添加mock环境,根据环境变量配置代理

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,32 @@
+let env = process.env.NODE_ENV.trim()
+
+let url = ""
+switch (env){
+  case 'mock':
+    url = "http://localhost:8090"
+    break;
+  case 'dev':
+    url = "http://localhost:8090"
+    break;
+  case 'production':
+  default:
+      url = ""
+    break;
+}
+let server = {}
+if(url) {
+  server = {
+    proxy:{
+      "/service": {
+        "target": url
+      }
+    }
+  }
+}
 module.exports = {
   devServer: {
     port: 3000,
-    proxy: {
-      "/service": {
-        "target": 'http://localhost:8090'
-      }
-    }
+    ...server
   },
   css: {
     extract: true
