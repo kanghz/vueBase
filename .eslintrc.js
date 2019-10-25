42c4bc6830997d107738375845f7524efe7f5b15
YCKJ1943 <kanghaizhen@cloudwalk.cn>
2019-10-25 15:21:25 +0800

    eslint test

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   "plugins": ["prettier"],
   "globals": {
-    "React": "readable"
+    "Vue": "readable"
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,6 @@ export default {
   components:{
     myHeader,
     sidebar
-    ad
   }
 }
 </script>
