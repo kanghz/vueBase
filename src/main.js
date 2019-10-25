5b958f6faab690b1c4599ad8c64792e13723c346
YCKJ1943 <kanghaizhen@cloudwalk.cn>
2019-10-25 17:13:21 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from "vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import "@/scss/common.scss";
-import "./router/permission.js";
+import Vue from "vue"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import "@/scss/common.scss"
+import "./router/permission.js"
 
-import App from "./App.vue";
+import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 
-Vue.use(ElementUI);
-Vue.config.productionTip = false;
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
